bbeb3551bb324befb77c74c4d54e45288d88052f
zalviandyr <zukronalviandy@gmail.com>
2021-06-15 14:32:14 +0700

    change url

--- lib/core/services/services.dart
+++ lib/core/services/services.dart
@@ -3,10 +3,10 @@ export 'transaction_service.dart';
 export 'user_service.dart';
 export 'chats_service.dart';
 
-String _baseUrl = '192.168.56.1:9000';
-Uri url(String path, [Map<String, dynamic>? params]) =>
-    Uri.http(_baseUrl, path, params);
+// String _baseUrl = '192.168.56.1:9000';
+// Uri url(String path, [Map<String, dynamic>? params]) =>
+//     Uri.http(_baseUrl, path, params);
 
-// String _baseUrl = 'kang-galon.herokuapp.com';
-// Uri url(String path, [Map<String, dynamic> params]) =>
-//     Uri.https(_baseUrl, path, params);
+String _baseUrl = 'kang-galon.herokuapp.com';
+Uri url(String path, [Map<String, dynamic>? params]) =>
+    Uri.https(_baseUrl, path, params);
